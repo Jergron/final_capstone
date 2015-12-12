23603e16dc06d0d44018fd34ca8a31f668f35d9a
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-12-12 12:37:20 -0600

    Implemented list of entries tests

--- DripScript.Tests/Models/DSUserTests.cs
+++ DripScript.Tests/Models/DSUserTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DripScript.Models;
+using System.Collections.Generic;
 
 namespace DripScript.Tests.Models
 {
@@ -30,6 +31,24 @@ namespace DripScript.Tests.Models
             Assert.AreEqual("Jeremy", a_user.FirstName);
             Assert.AreEqual("Grondahl", a_user.LastName);
             Assert.AreEqual("I am a software developer", a_user.Description);
+        }
+
+        [TestMethod]
+        public void DSUserEnsureUserHasJournalEntries()
+        {
+            // Arrange 
+            List<JournalEntry> list_of_entries = new List<JournalEntry>
+            {
+                new JournalEntry {Date = DateTime.Now, Title ="My Life", Body = "This is how I like to live my life" },
+                new JournalEntry {Date = DateTime.Now, Title ="Funny Bones", Body = "I couldn't believe how funny clowns could be!" }
+            };
+            DSUser a_user = new DSUser { FirstName = "Jeremy", LastName = "Grondahl", Entries = list_of_entries };
+
+            // Act
+            List<JournalEntry> actual_entries = a_user.Entries;
+
+            // Assert
+            CollectionAssert.AreEqual(list_of_entries, actual_entries); 
         }
     }
 }
