bf840423bda1e284f0ff239ecb44618b625f13dc
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-12-12 12:39:15 -0600

    Implemented date getter and setter

--- DripScript/Models/JournalEntry.cs
+++ DripScript/Models/JournalEntry.cs
@@ -6,14 +6,15 @@ using System.Web;
 
 namespace DripScript.Models
 {
-    public class JournalEntry
+    public class JournalEntry : DSUser
     {
-
         [Key]
         public int EntryId { get; set; }
         public string Title { get; set; }
+        [Required]
         public string Body { get; set; }
-
+        public DateTime Date { get; set; }
+        public string Author { get { return FirstName + " " + LastName; }}
         public bool isPublic()
         {
             return true;
