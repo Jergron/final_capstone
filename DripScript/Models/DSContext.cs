aca7496c71e52956127914084d794f09724eed3e
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-12-14 09:51:18 -0600

    Added context class to project

---
+++ DripScript/Models/DSContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DripScript.Models
+{
+    public class DSContext : ApplicationDbContext
+    {
+        public virtual DbSet<DSUser> DSUsers { get; set; }
+        public virtual DbSet<JournalEntry> Entries { get; set; }
+    }
+}
