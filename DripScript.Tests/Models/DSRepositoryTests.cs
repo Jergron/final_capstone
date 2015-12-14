2c32bd19df596f6cf8c5fb8babf2554fde19c305
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-12-14 09:45:51 -0600

    removed search by name method

---
+++ DripScript.Tests/Models/DSRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DripScript.Models;
+using Moq;
+using System.Data.Entity;
+
+namespace DripScript.Tests.Models
+{
+    [TestClass]
+    public class DSRepositoryTests
+    {
+        private Mock<DSContext> mock_context = new Mock<DSContext>();
+        private Mock<DbSet<DSUser>> mock_set = new Mock<DbSet<DSUser>>();
+        private Mock<DbSet<JournalEntry>> mock_entry_set = new Mock<DbSet<JournalEntry>>();
+         
+        private DSRepository repository = new DSRepository();
+
+
+        [TestMethod]
+        public void DSContextEnsureICanCreateInstance()
+        {
+            DSContext context = new DSContext();
+            Assert.IsNotNull(context);
+        }
+
+        [TestMethod]
+        public void DSRepositoryEnsureICanCreatInstance()
+        {
+            Assert.IsNotNull(repository);
+        }
+    }
+}
