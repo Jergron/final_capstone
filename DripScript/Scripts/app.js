cf9cb77c1fba4d5032dace0145fc806dd0413215
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-12-24 12:16:39 -0600

    Implemented Angular to views

---
+++ DripScript/Scripts/app.js
@@ -0,0 +1,17 @@
+﻿var app = angular.model("drip", []);
+
+app.controller("Controller", ["$scope", "$http", function ($scope, $http) {
+    $scope.test = "test variable";
+
+    $scope.deleteUsers = function () {
+        $http.delete("/api/DripScriptAPI")
+            .success(function (data) {
+                alert("Deleting Users Yay!");
+            })
+            .error(function (error) { alert(error.error) });
+    }
+
+    $scope.hello = function () {
+        $scope.test = "Hello World!"
+    }
+}]);
