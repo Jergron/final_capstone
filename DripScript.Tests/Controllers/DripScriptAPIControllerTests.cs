53a5fd3ef94875dea17dacb2e2e2acf0603d943e
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-12-31 15:45:22 -0600

    Implemented get method test for WebAPI controller. Test Passed

---
+++ DripScript.Tests/Controllers/DripScriptAPIControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Mvc;
+using DripScript.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DripScript.Tests
+{
+    [TestClass]
+    public class DripScriptAPIControllerTests
+    {
+        [TestMethod]
+        public void DripScriptAPIControllerEnsureICanCallGetAction()
+        {
+            // Arrange
+            DripScriptAPIController my_controller = new DripScriptAPIController();
+            string expected_output = "Hello World!";
+
+            // Act
+            string actual_output = my_controller.Get();
+
+            // Assert
+            Assert.AreEqual(expected_output, actual_output);
+        }
+    }
+}
