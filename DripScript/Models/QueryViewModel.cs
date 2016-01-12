233ea1826812b6e1fc9f9c8ca4cbcb8cfaef2133
Jeremy Grondahl <jeremygrondahl@gmail.com>
2016-01-11 23:58:47 -0600

    Created class to query search.

---
+++ DripScript/Models/QueryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DripScript.Models
+{
+    public class QueryViewModel
+    {
+        public string Book { get; set; }
+        public string Chapter { get; set; }
+        public string Verse { get; set; }
+    }
+}
